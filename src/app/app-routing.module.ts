6f0ed785b25135e745cc3d7ebfcd159db1a382eb
hai pham <phamhoangtrunghai@gmail.com>
2019-02-20 23:23:10 +0100

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes }  from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: 'dashboard', component: AppComponent },
   { path: 'about', component: AboutComponent },
   { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: '**', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
  
 @NgModule({
