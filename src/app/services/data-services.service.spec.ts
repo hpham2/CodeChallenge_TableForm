c7342614c327e22ca1e350bd1bcafd9df25d0c63
hai pham <phamhoangtrunghai@gmail.com>
2019-02-21 21:23:21 +0100

    spec files

--- src/app/services/data-services.service.spec.ts
+++ src/app/services/data-services.service.spec.ts
@@ -3,10 +3,15 @@ import { TestBed } from '@angular/core/testing';
 import { DataServicesService } from './data-services.service';
 
 describe('DataServicesService', () => {
+  // let service: DataServicesService;
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
+  it('should have id', () => {
     const service: DataServicesService = TestBed.get(DataServicesService);
-    expect(service).toBeTruthy();
+    // expect(service).toBeTruthy();
+    service.getModel().subscribe(val => {
+      let firstElement = val[0];
+      expect(firstElement.city).toEqual("Neftegorsk");
+    })
   });
 });
--- src/app/services/data-services.service.ts
+++ src/app/services/data-services.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import 'rxjs/Rx';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +14,7 @@ export class DataServicesService {
  * @function
  */
   getModel() {
-    let model = this.http.get('assets/model.json')
+    let model = this.http.get('assets/model.json');
     return model;
   }
 }
